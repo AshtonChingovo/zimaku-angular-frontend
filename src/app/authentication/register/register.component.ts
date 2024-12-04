ebd15cf87a3a0ea034af44820f86919cc3c66823
Ashton <ashton@Mac-14.local>
2024-12-04 23:48:14 +0200

    Add Registration form to API connection

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { RegisterService } from './register.service';
+import { HttpClientModule } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { AuthResponse } from '../model/auth-response.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, HttpClientModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+
+  private authResponseSubject: Subscription;
+  authResponse: AuthResponse;
+
+  constructor(private registerService: RegisterService){}
+
+  ngOnInit(): void {
+    this.authResponseSubject = this.registerService.authResponseSubject.subscribe( response => {
+      this.authResponse = response
+
+      if(this.authResponse.isSuccessfull){
+
+      }
+    })
+  }
+
+  onSubmit(registerForm: NgForm){
+    this.registerService.register({
+        firstname: "user2",
+        surname: "string",
+        email: "",
+        password: "string"
+      }
+    );
+  }
 
 }
+
+
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { importProvidersFrom } from '@angular/core';
 import { AppRoutingModule } from './app/app-routing.module';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(AppRoutingModule)
+    importProvidersFrom(AppRoutingModule),
+    provideHttpClient()
   ]
 })
   .catch((err) => console.error(err));
