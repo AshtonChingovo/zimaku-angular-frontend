b0c8b102a6824cf2efceca46d8c049e22f274293
Ashton <ashtonchingovo@gmail.com>
2024-12-12 22:26:58 +0200

    Add Pagination numbering logic to API link

--- src/app/production/chicks/records/records.component.ts
+++ src/app/production/chicks/records/records.component.ts
@@ -18,10 +18,13 @@ export class RecordsComponent implements OnInit{
   apiResponse: APIResponse
   chicksResponseModel: ChicksAPIResponseModel
 
+  isLoading = true
+
   // pagination
-  minPage: number
-  currentPage: number
-  maxPage: number
+  pages = []
+  minPage = 0
+  currentPage = 0
+  maxPage = 0
   isStartEnabled: boolean
   isPrevEnabled: boolean
   isNextEnabled: boolean
@@ -32,7 +35,7 @@ export class RecordsComponent implements OnInit{
   ngOnInit(): void {
 
     this.chicksService.getChicks({
-      page: 0,
+      page: 5,
       pageSize: 5,
       sortBy: "id"
     })
@@ -43,12 +46,19 @@ export class RecordsComponent implements OnInit{
 
       if(this.apiResponse.isSuccessful){
         this.chicksResponseModel = this.apiResponse.body
+
+        this.setUpPagination()
       }
+
+      this.isLoading = false
 
     })
   }
 
   getChicks(page: string){
+
+    this.isLoading = true
+
     this.chicksService.getChicks({
       page: 8,
       pageSize: 5,
@@ -56,7 +66,7 @@ export class RecordsComponent implements OnInit{
     })
   }
 
-  pagination(){
+  setUpPagination(){
     this.currentPage = this.chicksResponseModel.currentPage
 
     if(this.chicksResponseModel.first && this.chicksResponseModel.last){
@@ -65,18 +75,22 @@ export class RecordsComponent implements OnInit{
     }
     else if(this.chicksResponseModel.first){
       this.minPage = this.currentPage
+      this.maxPage = this.currentPage
+
       this.isStartEnabled = false
       this.isPrevEnabled = false
 
-      this.maxPage += (this.currentPage + 2 <= this.chicksResponseModel.pageSize) ? 2 : 1
+      this.maxPage += (this.currentPage + 2 <= this.chicksResponseModel.totalPages) ? 2 : (this.currentPage + 1 <= this.chicksResponseModel.totalPages) ? 1 : 0 
 
     }
     else if(this.chicksResponseModel.last){
       this.maxPage = this.currentPage
+      this.minPage = this.currentPage
+
       this.isNextEnabled = false
       this.isLastEnabled = false
 
-      this.minPage -= (this.currentPage - 2 >= 1) ? 2 : 1
+      this.minPage -= (this.currentPage - 2 > 0) ? 2 : (this.currentPage - 1 > 0) ? 1 : 0 
 
     }
     else{
@@ -95,6 +109,18 @@ export class RecordsComponent implements OnInit{
       }
     }
 
+    console.log(this.minPage + " -  " + this.currentPage + " - " + this.maxPage + " - " + this.chicksResponseModel.totalPages)
+
+    this.setUpPages()
+
+  }
+
+  setUpPages(){
+    this.pages = []
+    var i: number
+    for(i = this.minPage; i <= this.maxPage; ++i){
+      this.pages.push(i)
+    }
   }
 
 }
