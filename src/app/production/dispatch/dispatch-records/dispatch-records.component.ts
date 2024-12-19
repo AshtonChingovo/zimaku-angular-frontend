052cd902d48525c16a63a95e5af3ba09481ef439
Ashton <ashtonchingovo@gmail.com>
2024-12-19 12:48:07 +0200

    Add Production Dispatch UI

---
+++ src/app/production/dispatch/dispatch-records/dispatch-records.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dispatch-records',
+  standalone: true,
+  imports: [],
+  templateUrl: './dispatch-records.component.html',
+  styleUrl: './dispatch-records.component.css'
+})
+export class RecordsComponent {
+
+}
---
+++ src/app/production/dispatch/dispatch-stock/dispatch-stock.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dispatch-stock',
+  standalone: true,
+  imports: [],
+  templateUrl: './dispatch-stock.component.html',
+  styleUrl: './dispatch-stock.component.css'
+})
+export class DispatchStockComponent {
+
+}
